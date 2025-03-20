adfc1f1972c13c6eaa0b457b0a2154cee7039298
xxxinyu0707 <131528787+xxxinyu0707@users.noreply.github.com>
2025-03-20 18:08:23 +0800

    Add files via upload

---
+++ assignment4/homework1/homework1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1
+{
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+
+        public Node<T> Head
+        {
+            get { return head; }
+        }
+
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        // Node 类 (内部类)
+        public class Node<T>
+        {
+            public T Data { get; set; }
+            public Node<T> Next { get; set; }
+
+
+            public Node(T data)
+            {
+                Data = data;
+                Next = null;
+            }
+
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                action(current.Data);
+                current = current.Next;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> numbers = new GenericList<int>();
+            numbers.Add(5);
+            numbers.Add(2);
+            numbers.Add(8);
+            numbers.Add(1);
+
+
+            // 打印链表元素
+            Console.WriteLine("链表元素:");
+            numbers.ForEach(num => Console.WriteLine(num));
+
+
+            // 求最大值
+            int max = int.MinValue;
+            numbers.ForEach(num => max = Math.Max(max, num));
+            Console.WriteLine("最大值: " + max);
+
+
+            // 求最小值
+            int min = int.MaxValue;
+            numbers.ForEach(num => min = Math.Min(min, num));
+            Console.WriteLine("最小值: " + min);
+
+
+            // 求和
+            int sum = 0;
+            numbers.ForEach(num => sum += num);
+            Console.WriteLine("总和: " + sum);
+        }
+    }
+}
---
+++ assignment4/homework2/homework2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace homework2
+{
+    public class Clock
+    {
+        // 定义事件
+        public event EventHandler Tick; // 嘀嗒事件
+        public event EventHandler Alarm; // 响铃事件
+
+
+        private int alarmHour;
+        private int alarmMinute;
+
+
+        public Clock(int alarmHour, int alarmMinute)
+        {
+            this.alarmHour = alarmHour;
+            this.alarmMinute = alarmMinute;
+        }
+
+
+        // 触发嘀嗒事件的方法
+        protected virtual void OnTick(EventArgs e)
+        {
+            Tick?.Invoke(this, e); // 如果有订阅者，则触发 Tick 事件
+        }
+
+
+        // 触发响铃事件的方法
+        protected virtual void OnAlarm(EventArgs e)
+        {
+            Alarm?.Invoke(this, e); // 如果有订阅者，则触发 Alarm 事件
+        }
+
+
+        // 运行闹钟
+        public void Run()
+        {
+            while (true)
+            {
+                DateTime now = DateTime.Now;
+
+
+                // 触发嘀嗒事件
+                OnTick(EventArgs.Empty);
+
+
+                // 检查是否到达闹钟时间
+                if (now.Hour == alarmHour && now.Minute == alarmMinute && now.Second == 0)
+                {
+                    OnAlarm(EventArgs.Empty);
+                    break; // 闹钟响铃后停止
+                }
+
+
+                Thread.Sleep(1000); // 每秒嘀嗒一次
+            }
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 创建一个闹钟，设定闹钟时间
+            Console.WriteLine("设置闹钟时间(小时):");
+            int alarmHour = int.Parse(Console.ReadLine());
+            Console.WriteLine("设置闹钟时间(分钟):");
+            int alarmMinute = int.Parse(Console.ReadLine());
+
+
+            Clock clock = new Clock(alarmHour, alarmMinute);
+
+
+            // 订阅嘀嗒事件
+            clock.Tick += (sender, e) =>
+            {
+                Console.WriteLine("Tick: " + DateTime.Now.ToString("HH:mm:ss"));
+            };
+
+
+            // 订阅响铃事件
+            clock.Alarm += (sender, e) =>
+            {
+                Console.WriteLine("Alarm! Alarm!");
+            };
+
+
+            // 运行闹钟
+            clock.Run();
+        }
+    }
+}
